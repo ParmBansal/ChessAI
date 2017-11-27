0d26409c152b135a3092219804c3d85fc5f66074
Parm Bansal <P4rm8799@gmail.com>
2017-11-27 12:35:39 -0500

    Got rid of unwanted code.

--- ChessAI-master/ChessAI/src/chessai/Knight.java
+++ ChessAI-master/ChessAI/src/chessai/Knight.java
@@ -10,10 +10,8 @@ package chessai;
  * @author Ryan
  */
 public class Knight extends Piece{
-    public String textRepresentation;
     public Knight(boolean c, int x, int y,String a) {
 	super(c, x, y, a);
-	//textRepresentation = "N";
     }
     
 }
