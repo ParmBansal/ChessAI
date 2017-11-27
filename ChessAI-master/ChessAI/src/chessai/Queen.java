98c1b7c604ea8ab6bda6bdcf7f394fd096e973ed
Parm Bansal <P4rm8799@gmail.com>
2017-11-27 12:33:02 -0500

    Add files via upload

---
+++ ChessAI-master/ChessAI/src/chessai/Queen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessai;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Queen extends Piece{
+    public String textRepresentation;
+    public Queen(boolean c, int x, int y,String a) {
+	super(c, x, y,a);
+	//System.out.println(1);
+	//textRepresentation = "Q";
+	
+    }
+    
+}
